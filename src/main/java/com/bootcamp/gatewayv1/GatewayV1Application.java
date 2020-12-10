26f7009c40daa2ab51aab27f9159dad9e25fce5e
YaniraMurriel <yaniscase@hotmail.com>
2020-12-10 16:44:25 -0500

    gateway v1

---
+++ src/main/java/com/bootcamp/gatewayv1/GatewayV1Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.gatewayv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/gatewayv1/GatewayV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.gatewayv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
